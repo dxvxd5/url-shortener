f79d2bcc6f4de48b82985e9912045c8b93462aa5
David Kragbé <42718923+dxvxd5@users.noreply.github.com>
2024-12-16 00:38:53 +0000

    remove faulty code from client

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,51 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-
-// // Example.jsx
-// const MyComponent = () => <div>Hello, world!</div>;
-
-// export default MyComponent;
-
-// const useMyHook = () => {
-//   if (true) {
-//     const [state, setState] = React.useState(0); // Invalid usage
-//   }
-// };
-
-
-import { useEffect } from 'react';
-
-const MyComponent = ({ value }) => {
-  useEffect(() => {
-    console.log(value); // Missing `value` in dependency array
-  }, []);
-  return <div>Hello</div>;
-};
-
-export default MyComponent;
-
-
-
-
-// Example.jsx
-// function MyComponent({ value }) {
-//   if (value == 0) { // Should trigger an error for `eqeqeq`
-//     console.log('Loose equality');
-//   }
-
-//   return <div>Hello, world!</div>;
-// }
-
-// export default MyComponent;
-
-
-
-
-
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <div>This is the root of the app.</div>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
