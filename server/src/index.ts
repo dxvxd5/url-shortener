b58a796209b4963d3400f995c267897e66ef258a
David Kragbé <42718923+dxvxd5@users.noreply.github.com>
2024-12-16 00:53:39 +0000

    remove faulty code from server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,32 +1,4 @@
 import express from 'express';
-import pluginJs from '@eslint/js';
-
-console.log('is it working');
-
-function unsafeExample() {
-  return 'unsafe'; // This should trigger an error if returned without proper typing.
-}
-
-const button = document.querySelector('button');
-button?.addEventListener('click', async () => {
-  // Mistakenly passing an async function directly to an event handler
-  await fetch('/api');
-});
-
-async function example() {
-  Promise.resolve('value'); // This should trigger an error because the promise is not awaited.
-}
-
-const item = new Object();
-const s = '5';
-
-const age: number = 30;
-
-if (s == '5') {
-  console.log(age);
-}
-
-const name = `Capt. Janeway`;
 
 const app = express();
 const port = 8080;
