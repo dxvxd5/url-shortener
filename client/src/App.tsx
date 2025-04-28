57ba7e33a51545f490b8ea5a1c7407908a9e802c
David Kragbé <42718923+dxvxd5@users.noreply.github.com>
2025-04-28 03:09:29 +0100

    refactor App component structure and styles for improved layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,15 @@
+import styles from './styles/App.module.css';
+
 export const App = () => {
   return (
-    <div>
-      <h1>My React App</h1>
-      <p>Welcome Davids React application!</p>
-      <button>try reset</button>
+    <div className={styles['app-container']}>
+      <header className={styles['app-header']}>
+        <h1>Url Shortener</h1>
+        <p>
+          A simple and efficient tool to shorten your long URLs for easy sharing
+          and tracking.
+        </p>
+      </header>
     </div>
   );
 };
